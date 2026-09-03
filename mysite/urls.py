654ef1406db7346856b5de0463b07d7f98d48e0d
Wolfidy <lfernandezbazzini@alumno.huergo.edu.ar>
2026-09-03 15:37:42 -0300

    termine 8

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,14 @@
-from django.contrib import admin
+from django.conf import admin, settings
 from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
