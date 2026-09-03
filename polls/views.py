2e5ae2a07bc92198226386b36ad7dfcd74caab72
Wolfidy <lfernandezbazzini@alumno.huergo.edu.ar>
2026-09-03 15:04:22 -0300

    termine el 5

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Choice, Question
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -36,3 +37,22 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
